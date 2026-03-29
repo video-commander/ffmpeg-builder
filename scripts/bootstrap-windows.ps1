8e76b6abff91f8a3600db55aba7f06fc126fba9f
Alfred Gutierrez <alf.g.jr@gmail.com>
2026-03-29 22:49:02 +0900

    feat: windows build fixes

--- scripts/bootstrap-windows.ps1
+++ scripts/bootstrap-windows.ps1
@@ -1,5 +1,7 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
-choco install -y msys2
+
+winget install --id MSYS2.MSYS2 -e --source winget
+
 # Install toolchain + deps inside MSYS2
-& C:\tools\msys64\usr\bin\bash -lc "pacman -Sy --noconfirm --needed base-devel git nasm yasm cmake ninja mingw-w64-x86_64-toolchain mingw-w64-x86_64-pkgconf"
+& C:\msys64\usr\bin\bash -lc "pacman -Sy --noconfirm --needed base-devel git nasm yasm cmake ninja mingw-w64-x86_64-toolchain mingw-w64-x86_64-pkgconf"
